d0da32f9d58b1e1c85cfbdafaec8ca41725a1b36
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-12-26 19:51:57 -0800

    commit tree 978ce6749128b51847db841cb1b83260b132ec88

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  #returns an Array of the full names of each actor associated with a show. Remember, a show should have many actors through characters.
+  def actors_list
+    self.actors.pluck(:full_name)
+  end
 end
